de90339f1f8cab22f849162b4cc10751e7f41cf2
Ahryxx <phvanthanh06@gmail.com>
2025-11-25 16:55:06 +0700

    Thêm Module quản lý Categories: Tạo cấu trúc module, bao gồm các controller, service provider, routes, và views; thêm chức năng quản lý danh mục với CRUD cơ bản.

---
+++ Modules/Categories/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Categories\Http\Controllers\CategoriesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group([], function () {
+    Route::resource('categories', CategoriesController::class)->names('categories');
+});
